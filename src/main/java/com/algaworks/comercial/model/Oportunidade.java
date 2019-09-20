76a18b4cd2c3329ce1a298a9fbe4628e9c3aee6f
Wagner Andrade <wagnerandrade_df@hotmail.com>
2019-09-20 15:31:23 -0300

    Metodo adicionar com PostMapping, ResponseStatus, Validação de tamanho

--- src/main/java/com/algaworks/comercial/model/Oportunidade.java
+++ src/main/java/com/algaworks/comercial/model/Oportunidade.java
@@ -2,10 +2,14 @@ package com.algaworks.comercial.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Oportunidade {
@@ -14,8 +18,16 @@ public class Oportunidade {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	private String descricao;
+	@NotEmpty
+	@Size(max = 80)
+	@Column(name = "nome_prospecto")
 	private String nomeProspecto;
+	
+	@NotEmpty
+	@Size(max = 200)
+	private String descricao;
+	
+	@Min(0)
 	private BigDecimal valor;
 	
 	public long getId() {
